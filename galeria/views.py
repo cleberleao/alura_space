feef5ebbdab2163c268ebda142a50391704f1e38
Cleber Leão <cleber.leao@external.stellantis.com>
2024-06-05 14:17:19 -0300

    update treinamento

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {
+            'nome': 'Nebulosa de Carina',
+            'legenda': 'webbtelescope.org / NASA / James Webb'
+        },
+        2: {
+            'nome': 'Galaxia de Andrômeda',
+            'legenda': 'NASA / ESA / J. Dalcanton'
+        },
+    }
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
